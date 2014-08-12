aefa0abb4a50de3f8d4ae0aeb00688e828fe5499
atrakeur <atrakeur@gmail.com>
2014-08-12 19:09:17 +0200

    Initial commit

---
+++ src/Atrakeur/Repository/AbstractRepository.php
@@ -0,0 +1,5 @@
+<?php namespace Atrakeur\Repository;
+
+abstract class AbstractRepository {
+
+}
---
+++ src/Atrakeur/Repository/Eloquent/AbstractEloquentRepository.php
@@ -0,0 +1,68 @@
+<?php namespace Atrakeur\Repository\Eloquent;
+
+use \Atrakeur\Repository\Eloquent\EloquentConverter;
+use \Illuminate\Database\Eloquent\Model as Eloquent;
+
+abstract class AbstractEloquentRepository {
+
+	protected $model;
+	protected $query;
+
+	public function __construct(Eloquent $model, EloquentConverter $converter)
+	{
+		$this->model     = $model;
+		$this->converter = $converter;
+		$this->resetScope();
+	}
+
+	public function boot()
+	{
+	}
+
+	public function resetScope()
+	{
+		$this->query = $this->model;
+		$this->boot();
+		return $this;
+	}
+
+	public function with(Array $array)
+	{
+		$this->query = $this->query->with($array);
+		return $this;
+	}
+
+	public function byField($field, $value)
+	{
+		$this->query = $this->query->where($field, '=', $value);
+		return $this;
+	}
+
+	public function byId($ident)
+	{
+		$this->query = $this->query->where('id', '=', $ident);
+		return $this;
+	}
+
+	public function paginate($itemCount)
+	{
+		$data = $this->query->paginate($itemCount);
+		$this->resetScope();
+		return $this->converter->convert($data);
+	}
+
+	public function getOne()
+	{
+		$data = $this->query->first();
+		$this->resetScope();
+		return $this->converter->convert($data);
+	}
+
+	public function getMany()
+	{
+		$data = $this->query->get();
+		$this->resetScope();
+		return $this->converter->convert($data);
+	}
+
+}
